37c13c68100a5eb3d8ea653ae26f1b31453154dd
tonningp <tonningp@cis.vvc.edu>
2017-11-07 20:05:37 -0800

    update

--- worked_examples/stdev/example1.cpp
+++ worked_examples/stdev/example1.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
+#include<string>
 #include<vector>
 
-
-void print_vec(std::vector<double>& vec)
+void print_vector(const std::vector<double>& vec)
 {
     for(int i=0;i<vec.size();i++)
         std::cout << vec[i] << " ";
+    std::cout << std::endl;
 }
 
-double average(std::vector<double> v)
+void print_vector(const std::vector<std::string>& vec)
 {
-    double sum=0.0;
-    for(int i=0;i<v.size();i++)
-        sum += v[i];
-
-    return sum / v.size();
+    for(int i=0;i<vec.size();i++)
+        std::cout << vec[i] << " ";
+    std::cout << std::endl;
 }
-
+double average(const std::vector<double> &vec)
+{
+    double sum = 0.0;
+    for(int i=0;i<vec.size();i++)
+        sum += vec[i];
+    return sum / vec.size();
+}
 int main()
 {
-    std::vector<double> v;// = {10.2,10.4,10.6};
-    v.push_back(10.2);
-    v.push_back(10.4);
-    v.push_back(10.6);
-    std::cout << "average is:" << average(v)
+    std::vector<double> v = {10.2,10.4,10.6};
+    //print_vector(v);
+    //print_vector({10.2,10.4,10.6});
+    std::vector<std::string> sv;
+    std::cout << "average is:" << average(v);
+    std::cout << std::endl;
 
     return 0;
 }
--- worked_examples/stdev/main.cpp
+++ worked_examples/stdev/main.cpp
@@ -7,73 +7,56 @@
  * 3.  calculate the variation from the mean and sum the value of the variation squared
  * 4.  calculate the square root of the sum of the squared variations divided by the number of values - 1
  */
-
-
 #include<iostream>
-#include<vector>
 #include<cmath>
+#include<vector>
 
-void read_data(std::vector<double>& vec) 
-{ 
-    /*
+void read_data(std::vector<double>& vec)
+{   
     vec.push_back(727.7);
     vec.push_back(1086.5);
     vec.push_back(1091.0);
     vec.push_back(1361.3);
     vec.push_back(1490.5);
     vec.push_back(1956.1);
-    */
-    //std::cout << "Enter the data (Ctrl+D to end):";
-    double x=0.0;
-    while( std::cin >> x )
-        vec.push_back(x);
+}
 
-} 
-
-void print_vec(std::vector<double>& vec)
+void print_vector(const std::vector<double>& vec)
 {
     for(int i=0;i<vec.size();i++)
         std::cout << vec[i] << " ";
+    std::cout << std::endl;
 }
-/**
- * calculate the sum of the variances from the mean squared
- * 
- * @param vec vector of double data
- * @param mean the mean of the sample data
- * @return the sum of the variances squared
- */
-double sum_variance(std::vector<double> vec,double mean)
+
+double average(const std::vector<double>& vec)
 {
-    double sum=0.0;
-    for(int i=0;i<vec.size();i++)
-        sum += pow((vec[i] - mean),2);
+    double sum = 0.0;
+    for(int i=0;i < vec.size();i++)
+        sum += vec[i];
+    return sum / vec.size();
+}
+
+double sum_of_variation(const std::vector<double>& vec)
+{
+    double sum = 0.0;
+    double mean = average(vec);
+    for(int i=0;i < vec.size();i++)
+        sum += pow(vec[i]-mean,2);
     return sum;
 }
-
-double average(std::vector<double> v)
+double stddev(const std::vector<double>& vec)
 {
-    double sum=0.0;
-    for(int i=0;i<v.size();i++)
-        sum += v[i];
-
-    return sum / v.size();
+    return sqrt( sum_of_variation(vec) / (vec.size()-1) );
 }
-
-double stddev(std::vector<double> v)
-{
-    double avg = average(v);    
-    return sqrt(sum_variance(v,avg)/(v.size()-1));
-}
-
 int main()
+
 {
     std::vector<double> v;
     read_data(v);
+    print_vector(v);
     std::cout << stddev(v) << std::endl;
     return 0;
 }
-
-
 
 
 
